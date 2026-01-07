caa232f9091ebb75cd32dc1b51b60c918f8b7a63
Hydrees <idrisbnabdussalam@gmail.com>
2026-01-07 15:09:24 +0100

    icon update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
+import icon from "../assets/react.svg"
 export default function Header() {
     return (
         <header>
             <div className="logoName">
-            <img src="src/assets/react.svg" alt="chef logo"/>
-            <h1>Chef Claude</h1>
+                <img src={icon} alt="chef logo" />
+                <h1>Chef Claude</h1>
             </div>
             <p className="note">...Generate Your Recipe With Minimum Of 4 Ingredients..</p>
         </header>
